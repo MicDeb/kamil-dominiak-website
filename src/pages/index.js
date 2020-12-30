97a57f60f5d93d80fe2cd43677357c3dfaabf9b5
Michał Dębek <michal.debek@silksh.pl>
2020-12-30 15:11:02 +0100

    Add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import Separator from 'src/components/Separator';
 
 const Index = () => (
   <section className='section'>
     <Helmet title='Kamil Dominiak - official website' />
-
+    <div className='home-page-container'>
+      <h2 className='home-page__title'>Kamil Dominiak</h2>
+      <Separator />
+      <p className='home-page__subtitle'>Aktor | Wokalista | Trener wokalny</p>
+    </div>
   </section>
 );
 
